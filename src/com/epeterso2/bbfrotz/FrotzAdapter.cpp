603abfb8ff52d91cba19aba2672dd8bff525e7f7
Eric Peterson <epeterso2@gmail.com>
2012-07-23 06:44:00 -0400

    Continuing implementation

--- src/com/epeterso2/bbfrotz/FrotzAdapter.cpp
+++ src/com/epeterso2/bbfrotz/FrotzAdapter.cpp
@@ -45,11 +45,18 @@ FrotzAdapter::~FrotzAdapter()
 {
 }
 
+
 extern "C"
-void 	os_beep (int)
+void 	os_beep (int number)
 {
-	;
+	FrotzAdapter::getInstance()->os_beep(number);
 }
+
+void FrotzAdapter::os_beep(int number)
+{
+	emit beep(number);
+}
+
 
 extern "C"
 int  	os_char_width (zchar)
@@ -57,17 +64,30 @@ int  	os_char_width (zchar)
 	return 0;
 }
 
-extern "C"
-void 	os_display_char (zchar)
-{
-	;
-}
 
 extern "C"
-void 	os_display_string (const zchar *)
+void 	os_display_char (zchar zc)
 {
-	;
+	FrotzAdapter::getInstance()->os_display_char(zc);
 }
+
+void FrotzAdapter::os_display_char(zchar zc)
+{
+	emit display(convert(zc));
+}
+
+
+extern "C"
+void 	os_display_string (const zchar * zc)
+{
+	FrotzAdapter::getInstance()->os_display_string(zc);
+}
+
+void FrotzAdapter::os_display_string(const zchar * zc)
+{
+	emit display(convert(zc));
+}
+
 
 extern "C"
 void 	os_draw_picture (int, int, int)
@@ -81,11 +101,18 @@ void 	os_erase_area (int, int, int, int)
 	;
 }
 
+
 extern "C"
-void 	os_fatal (const char *)
+void 	os_fatal (const char * message)
 {
-	;
+	FrotzAdapter::getInstance()->os_fatal(message);
 }
+
+void FrotzAdapter::os_fatal(const char * message)
+{
+	emit fatal(QString(message));
+}
+
 
 extern "C"
 void 	os_finish_with_sample (int)
@@ -102,7 +129,12 @@ int  	os_font_data (int, int *, int *)
 extern "C"
 void 	os_init_screen (void)
 {
-	;
+	FrotzAdapter::getInstance()->os_init_screen();
+}
+
+void FrotzAdapter::os_init_screen(void)
+{
+	emit initScreen();
 }
 
 extern "C"
@@ -114,7 +146,7 @@ void 	os_more_prompt (void)
 extern "C"
 FILE *  os_path_open (char * filename, char * mode)
 {
-	;
+	return 0;
 }
 
 extern "C"
@@ -171,17 +203,30 @@ zword   os_read_mouse(void)
 	return 0;
 }
 
+
 extern "C"
 void 	os_reset_screen (void)
 {
-	;
+	FrotzAdapter::getInstance()->os_reset_screen();
 }
 
-extern "C"
-void 	os_restart_game (int)
+void FrotzAdapter::os_reset_screen(void)
 {
-	;
+	emit resetScreen();
 }
+
+
+extern "C"
+void 	os_restart_game (int code)
+{
+	FrotzAdapter::getInstance()->os_restart_game(code);
+}
+
+void FrotzAdapter::os_restart_game(int code)
+{
+	emit restartGame(code);
+}
+
 
 extern "C"
 void 	os_scroll_area (int, int, int, int, int)
@@ -231,14 +276,32 @@ int  	os_string_width (const zchar *)
 	return 0;
 }
 
+
 extern "C"
 void	os_init_setup (void)
 {
-	;
+	FrotzAdapter::getInstance()->os_init_setup();
 }
+
+void FrotzAdapter::os_init_setup(void)
+{
+	emit initSetup();
+}
+
 
 extern "C"
 int	os_speech_output(const zchar *)
 {
 	return 0;
+}
+
+
+QString FrotzAdapter::convert(zchar zc)
+{
+	return QString(zc);
+}
+
+QString FrotzAdapter::convert(const zchar * zc)
+{
+	return QString("X");
 }
--- src/com/epeterso2/bbfrotz/FrotzAdapter.h
+++ src/com/epeterso2/bbfrotz/FrotzAdapter.h
@@ -10,6 +10,8 @@
 
 #include <QObject>
 
+#include "frotz.h"
+
 class FrotzAdapter: QObject
 {
 	Q_OBJECT
@@ -17,10 +19,55 @@ class FrotzAdapter: QObject
 public:
 	static FrotzAdapter * getInstance(void);
 
+	void 	os_beep (int);
+	int  	os_char_width (zchar);
+	void 	os_display_char (zchar);
+	void 	os_display_string (const zchar *);
+	void 	os_draw_picture (int, int, int);
+	void 	os_erase_area (int, int, int, int);
+	void 	os_fatal (const char *);
+	void 	os_finish_with_sample (int);
+	int  	os_font_data (int, int *, int *);
+	void 	os_init_screen (void);
+	void	os_init_setup (void);
+	void 	os_more_prompt (void);
+	int  	os_peek_colour (void);
+	int  	os_picture_data (int, int *, int *);
+	void 	os_prepare_sample (int);
+	void 	os_process_arguments (int, char *[]);
+	int		os_random_seed (void);
+	int  	os_read_file_name (char *, const char *, int);
+	zchar	os_read_key (int, int);
+	zchar	os_read_line (int, zchar *, int, int, int);
+	void 	os_reset_screen (void);
+	void 	os_restart_game (int);
+	void 	os_scroll_area (int, int, int, int, int);
+	void 	os_set_colour (int, int);
+	void 	os_set_cursor (int, int);
+	void 	os_set_font (int);
+	void 	os_set_text_style (int);
+	void 	os_start_sample (int, int, int, zword);
+	void 	os_stop_sample (int);
+	int  	os_string_width (const zchar *);
+	int		os_speech_output(const zchar *);
+
+public slots:
+
+signals:
+	void beep(int number);
+	void display(QString text);
+	void fatal(QString message);
+	void initScreen(void);
+	void initSetup(void);
+	void resetScreen(void);
+	void restartGame(int code);
+
 private:
 	static FrotzAdapter * instance;
 	FrotzAdapter();
 	virtual ~FrotzAdapter();
+	QString convert(zchar zc);
+	QString convert(const zchar * zc);
 };
 
 #endif /* FROTZADAPTER_H_ */
